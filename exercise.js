933bc2d05431b1041c10b65756791ef514a92674
ALARMich <55944078+alarmichanin@users.noreply.github.com>
2022-01-27 02:31:54 +0200

    +1 more task from codewars

--- exercise.js
+++ exercise.js
@@ -84,3 +84,25 @@ var countBits = function (n) {
     n.toString(2).split("").map(num => { if (num == 1) count++ });
     return count;
 };
+
+
+/*
+Complete the method/function so that it converts dash/underscore 
+delimited words into camel casing. The first word within the output 
+should be capitalized only if the original word was capitalized 
+(known as Upper Camel Case, also often referred to as Pascal case).
+
+Examples
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+*/
+
+function toCamelCase(str) {
+    let res = str.split(/[\-\_]/).map((word, index) => {
+        if (index !== 0) {
+            word = word[0].toUpperCase() + word.slice(1);
+        }
+        return word;
+    })
+    return res.join("");
+}
