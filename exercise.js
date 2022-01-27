32eeaaa2ab4e958a6e92f3d54d782e566a6d275c
ALARMich <55944078+alarmichanin@users.noreply.github.com>
2022-01-27 02:05:05 +0200

    +1 task from codedwars

--- exercise.js
+++ exercise.js
@@ -64,3 +64,23 @@ var uniqueInOrder = function (iterable) {
     }
     return res;
 }
+
+
+
+
+/*
+Write a function that takes an integer as input, 
+and returns the number of bits that are equal to one 
+in the binary representation of that number. 
+You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, 
+so the function should return 5 in this case
+*/
+
+
+var countBits = function (n) {
+    let count = 0;
+    n.toString(2).split("").map(num => { if (num == 1) count++ });
+    return count;
+};
